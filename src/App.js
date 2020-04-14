d12492e01c7f1905d47d191308e8eab5c26397a7
rubensojunior <rubensojunior6@gmail.com>
2020-04-13 23:39:17 -0300

    implementing add repository

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import "./styles.css";
 
 function App() {
   const [repositories, setRepositories] = useState([]);
+  const [repository, setRepository] = useState({
+    title: "",
+    url: "",
+    techs: ["d"],
+  });
 
   useEffect(() => {
     api.get("repositories").then((response) => {
@@ -12,8 +17,26 @@ function App() {
     });
   }, []);
 
+  const handleChangeTitle = (event) => {
+    setRepository({ ...repository, title: event.target.value });
+  };
+
+  const handleChangeUrl = (event) => {
+    setRepository({ ...repository, url: event.target.value });
+  };
+
+  const handleChangeTechs = (event) => {
+    setRepository({
+      ...repository,
+      techs: [event.target.value],
+    });
+  };
+
   async function handleAddRepository() {
-    // TODO
+    api.post("repositories", repository).then((response) => {
+      alert(response.data.url);
+      setRepositories([...repositories, response.data]);
+    });
   }
 
   async function handleRemoveRepository(id) {
@@ -22,12 +45,34 @@ function App() {
 
   return (
     <div>
+      <form>
+        <input
+          type="text"
+          placeholder="Título"
+          value={repository.title}
+          onChange={handleChangeTitle}
+        />
+        <input
+          type="text"
+          placeholder="url"
+          value={repository.url}
+          onChange={handleChangeUrl}
+        />
+        <input
+          type="text"
+          placeholder="Tecnologia 1"
+          value={repository.techs[0]}
+          onChange={handleChangeTechs}
+        />
+        <button type="submit" onClick={handleAddRepository}>
+          Adicionar
+        </button>
+      </form>
       <ul data-testid="repository-list">
         {repositories.map((repository) => (
           <li key={repository.id}>{repository.title}</li>
         ))}
       </ul>
-      <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
 }
